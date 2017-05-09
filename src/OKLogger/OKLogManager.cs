c67cae4305201d75f7b6cfaa4f26ce4750640a41
Brian Kennedy <brian@vistaprint.com>
2017-05-09 18:34:29 -0400

    Fixed logger repository

--- src/OKLogger/OKLogManager.cs
+++ src/OKLogger/OKLogManager.cs
@@ -15,36 +15,33 @@ namespace OKLogger
 
         public static ILogger GetLogger(string name, string environment)
         {
-            var respositoryName = LogManager.GetAllRepositories().First().Name;
-
-            var log = LogManager.GetLogger(respositoryName, name);
+            
+            var log = LogManager.GetLogger(DefaultRepository, name);
             return new Logger(log, environment);
         }
 
         public static ILogger GetLogger(string name, string environment, object context)
         {
-            var respositoryName = LogManager.GetAllRepositories().First().Name;
 
-            var log = LogManager.GetLogger(respositoryName, name);
+            var log = LogManager.GetLogger(DefaultRepository, name);
             return new Logger(log, environment, context);
         }
 
         public static ILogger GetLogger<T>(string environment, object context)
         {
-            var log = LogManager.GetLogger(typeof(T));
+            var log = LogManager.GetLogger(DefaultRepository, typeof(T).Name);
             return new Logger(log, environment, context);
         }
 
         public static ILogger GetLogger<T>(string environment)
         {
-            var log = LogManager.GetLogger(typeof(T));
+            var log = LogManager.GetLogger(DefaultRepository, typeof(T).Name);
             return new Logger(log, environment);
         }
 
         public static void Configure(FileInfo config)
         {
             var logRepository =  LogManager.CreateRepository(DefaultRepository);
-
             XmlConfigurator.Configure(logRepository, config);
 
         }
