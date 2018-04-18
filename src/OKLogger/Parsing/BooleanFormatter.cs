dd51c9e85635c7080c4cfd8439fbab2197df18d1
Brian Kennedy <brian@vistaprint.com>
2018-04-18 15:07:42 -0400

    Added boolean formatter

---
+++ src/OKLogger/Parsing/BooleanFormatter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OKLogger.Parsing
+{
+    public class BooleanFormatter : IEntityFormatter
+    {
+        private HashSet<Type> HandledTypes = new HashSet<Type> {
+                typeof(bool), typeof(Boolean)
+            };
+
+        public bool Handles(Type t)
+        {
+            return HandledTypes.Contains(t);
+        }
+
+        public Dictionary<string, string> Format(object item, int depth)
+        {
+            if (item == null) return new Dictionary<string, string>() { { string.Empty, string.Empty } };
+
+            bool itemAsBoolean = (bool)item;
+            if(itemAsBoolean)
+            {
+                return new Dictionary<string, string>()
+                {
+                    { string.Empty, "true" }
+                };
+            }
+            else
+            {
+                return new Dictionary<string, string>()
+                {
+                    { string.Empty, "false" }
+                };
+            }
+
+        }
+    }
+}
